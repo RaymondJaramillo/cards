0f647984a417266fa9384c62e59c6866baea693e
raymondjaramillo <rjaramillo155@cnm.edu>
2020-01-24 09:43:04 -0700

    Added hash

--- src/edu/cnm/deepdive/model/Card.java
+++ src/edu/cnm/deepdive/model/Card.java
@@ -1,13 +1,18 @@
 package edu.cnm.deepdive.model;
 
+import java.util.Objects;
+
 public class Card {
+
 
   private final Suit suit;
   private final Rank rank;
+  private final int hash;
 
   public Card(Suit suit, Rank rank) {
     this.suit = suit;
     this.rank = rank;
+    hash = Objects.hash(suit, rank);
   }
 
   public Suit getSuit() {
@@ -22,6 +27,23 @@ public class Card {
   public String toString() {
     return rank.symbol() + suit.symbol();
   }
+
+  @Override
+  public int hashCode() {
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    boolean comparison = false;
+    if (obj == this) {
+      comparison = true;
+    } else if (obj instanceof Card && obj.hashCode() == hash) {
+      Card other = (Card) obj;
+      if (suit == ((Card) obj).suit && rank == ((Card)obj).rank);
+        comparison = true;
+    }
+    return comparison;
+  }
+
 }
-
-
